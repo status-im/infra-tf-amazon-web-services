c489779a47ffe13ae0f88b63c9725cc7c2a16a43
Jakub Sokołowski <jakub@status.im>
2020-07-07 20:30:52 +0200

    use Ubuntu 20.04 image
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- variables.tf
+++ variables.tf
@@ -45,7 +45,8 @@ variable data_vol_type {
 variable image_name {
   description = "Name of AMI image to use."
   type        = string
-  default     = "ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-20200112"
+  /* Use: aws ec2 describe-images --filters 'Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-focal-20.04*' */
+  default     = "ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-20200701"
 }
 
 variable image_owner {
