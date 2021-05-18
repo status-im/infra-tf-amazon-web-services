89b9a0f56a68db1386aa231100a6109b2014fb82
Jakub Sokołowski <jakub@status.im>
2021-05-18 13:54:13 +0200

    open 51820 UPD port for WireGuard VPN
    https://github.com/status-im/infra-hq/issues/58
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ locals {
   groups = distinct([local.data_center, "${var.env}.${local.stage}", var.group])
   /* always add SSH, Tinc, Netdata, and Consul to allowed ports */
   open_tcp_ports  = concat(["22", "655", "8000", "8301"], var.open_tcp_ports)
-  open_udp_ports  = concat(["655", "8301"], var.open_udp_ports)
+  open_udp_ports  = concat(["51820", "655", "8301"], var.open_udp_ports)
 }
 
 /* the image needs to be queried */
