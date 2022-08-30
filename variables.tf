777defe53d0877baf4d1a1acaf758a54b07e6d50
Jakub Sokołowski <jakub@status.im>
2022-08-30 13:18:41 +0200

    fix architecture of ubuntu image used
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- variables.tf
+++ variables.tf
@@ -50,7 +50,7 @@ variable "data_vol_iops" {
 variable "image_name" {
   description = "Name of AMI image to use."
   type        = string
-  default     = "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-arm64-server-20220810"
+  default     = "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20220810"
   /* Use: aws ec2 describe-images --filters 'Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-focal-20.04*' */
 }
 
