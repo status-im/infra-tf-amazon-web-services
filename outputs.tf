d012d2e77db87343bc47efd913e2a015854ec3f1
Jakub Sokołowski <jakub@status.im>
2024-03-14 16:09:03 +0100

    add public_ips_v6 to outputs
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "public_ips" {
   value = local.public_ips
 }
 
+output "public_ips_v6" {
+  value = flatten([for a in aws_instance.host : a.ipv6_addresses])
+}
+
 output "hostnames" {
   value = local.hostnames
 }
