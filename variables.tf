7667b642007d94d7d487f9c8b0fe40257aad0a13
Jakub Sokołowski <jakub@status.im>
2025-06-05 13:03:24 +0200

    variables: update ubuntu AMI image name
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- variables.tf
+++ variables.tf
@@ -56,8 +56,8 @@ variable "data_vol_iops" {
 variable "image_name" {
   description = "Name of AMI image to use."
   type        = string
-  default     = "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20240801"
-  /* Use: aws ec2 describe-images --filters 'Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-jammy-22.04*' */
+  default     = "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20250516"
+  /* Use: aws ec2 describe-images --filters 'Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64*' | jq -r '.Images[].Name' */
 }
 
 variable "image_owner" {
